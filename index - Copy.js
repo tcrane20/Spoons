f69dda6b1bab15d8368cfce57e7e2057df241607
tcrane20 <tscrane20@gmail.com>
2015-05-03 23:19:16 -0700

    Waiting to be merged
    Title

--- index - Copy.js
+++ index - Copy.js
@@ -41,7 +41,7 @@ var MAX_PLAYERS = 3;
 var openRoomID = 0; // Current room index that is accepting players
 var waitTime = 0;   // Amount of time remaining in the open room before a game forcibly starts
 // Collection of all existing rooms. Holds list of users connected to it, number of spoons on table, and a personal timer element.
-var rooms = {"0": {"users": {}, "spoons": 0, "timer": 0}};  
+var rooms = {"0": {"users": [], "spoons": 0, "timer": 0}};  
 // Collection of connected sockets. Format: USERNAME => SOCKET
 var userToSocket = {};
 
@@ -79,32 +79,22 @@ var shuffle = function (o) {
 var baseDeck = initDeck(deckSchema); // Deck Initialized
 
 // A new shuffled deck is created. The slice(0) ensures a clone of baseDeck so that baseDeck is not modified by the sort.
-//var shuffledDeck = shuffle(baseDeck.slice(0)); 
+var shuffledDeck = shuffle(baseDeck.slice(0)); 
 
-function fourOfKind(cards){
-	var card = cards[0],
-		value = card.value;
-	for (var i = 1; i < 4; i++){
-		card = cards[i];
-		if (card.value !== value)
-			return false;
-	}
-	return true;
-}
 
 function closeRoom(){
 	// Send messages to all users in the room
-	Object.keys(rooms[openRoomID].users).forEach(function (uname, index, users){
+	rooms[openRoomID].users.forEach(function (uname, index, users){
 		sock = userToSocket[uname];
 		sock.emit("playerIndex", index);
-		sock.emit("gameStart", users.length - 1);
+		sock.emit("gameStart");
 		sock.emit("usersInRoom", users);
 	});
 	// Prepare the cards for the room
 	prepGame(openRoomID);
 
 	openRoomID++;
-	rooms[openRoomID] = {"users": {}, "spoons": 0, "timer": 0};
+	rooms[openRoomID] = {"users": [], "spoons": 0, "timer": 0};
 }
 
 
@@ -112,19 +102,26 @@ function prepGame(roomID){
 	var room = rooms[roomID],
 		users = room.users;
 	var deck = shuffle(baseDeck.slice(0));
-
+	console.log(roomID);
+	console.log(room);
+	console.log(users);
 	// Deal out the players' hands
-	for (var i = 0; i < Object.keys(users).length; i++){
-		var name = Object.keys(users)[i],
-			user = users[name];
-		user.hand = deck.splice(0,4);
-		userToSocket[name].emit("playerHand", user.hand);
+	for (var i = 0; i < users.length; i++){
+		var hand = deck.splice(0,4);
+		console.log(hand); // PRINTS OUT USERNAME JUST FINE
+
+		sessionSockets.getSession(userToSocket[users[i]], function (err, session){
+
+			console.log(session.username); // UNDEFINED
+			session.hand = hand;
+			session.save();
+		});
 	}
 	// Give remaining deck to first player's pile
-	var dealerName = Object.keys(users)[0],
-		dealer = users[dealerName];
-	dealer.pile = deck;
-	userToSocket[dealerName].emit("updatePile", false);
+	sessionSockets.getSession(userToSocket[users[0]], function (err, session){
+		session.pile = deck;
+		session.save();
+	});
 }
 
 //=============================================================================
@@ -144,46 +141,43 @@ sessionSockets.on("connection", function (err, socket, session){
 		// If the user didn't input a name, don't process any further
 		if (!session.username) return;
 
-		console.log(session.username + " disconnected with socket id: " + socket.id);
+		console.log(session.username + "disconnected with socket id: " + socket.id);
 		// Delete presence of the username being logged in
 		delete userToSocket[session.username];
 		// Get the room this user was in
 		var roomid = session.room;
 		// Get all users in this room
 		var users = rooms[roomid].users;
-		// Delete the user from the room
-		delete rooms[roomid].users[session.username];
-		// No more users in the room, so we can delete it
-		if (Object.keys(users).length === 0 && roomid !== openRoomID){
-			delete rooms[roomid];
-		} else {
-			// Send message to all users in the room about disconnect
-			Object.keys(users).forEach(function (uname, index, players){
-				userToSocket[uname].emit("usersInRoom", players);
-				// If only one user left, remove the timer
-				if (players.length === 1)
-					userToSocket[uname].emit("time", 0);
-			});
-		}
+		// Find this user and remove them
+		var i = users.indexOf(session.username);
+		users.splice(i, 1);
+		// Send message to all users in the room about disconnect
+		users.forEach(function (uname, index, users){
+			userToSocket[uname].emit("usersInRoom", users);
+			// If only one user left, remove the timer
+			if (users.length === 1)
+				userToSocket[uname].emit("time", 0);
+		});
 	});
 
 	// Client, with accepted username, is looking for a room to join
-	socket.on("joinGame", function (username) {
+	socket.on("username", function (username) {
 		console.log("user " + username + " connected.");
 		// Associate username with socket object
 		userToSocket[username] = socket;
 		// Save user session by initalizing data
+		session.hand = [];
+		session.pile = [];
+		session.spoon = false;
 		session.room = openRoomID;
 		session.username = username;
 		session.uid = socket.id;
 		session.save();
 
-		var userData = {hand: [], pile: [], hasSpoon: false};
-
 		// Add user session to the open room
-		rooms[openRoomID].users[username] = userData;
+		rooms[openRoomID].users.push(username);
 		// If room is full now
-		var roomsize = Object.keys(rooms[openRoomID].users).length;
+		var roomsize = rooms[openRoomID].users.length;
 		if (roomsize === MAX_PLAYERS){
 			closeRoom();
 		} else {
@@ -193,83 +187,12 @@ sessionSockets.on("connection", function (err, socket, session){
 				waitTime = 0;
 			}
 			// Send message to all users in the room
-			Object.keys(rooms[openRoomID].users).forEach(function (user, index, users){
-				userToSocket[user].emit("usersInRoom", users);
-				userToSocket[user].emit("time", waitTime);
+			rooms[openRoomID].users.forEach(function (uname, index, users){
+				userToSocket[uname].emit("usersInRoom", users);
+				userToSocket[uname].emit("time", waitTime);
 			});
 		}
 	});
-
-//--------------------------------------------
-// In game messages
-//--------------------------------------------
-	// User is requesting the top card
-	socket.on("reqTopCard", function (){
-		var username = session.username,
-			roomid = session.room,
-			pile = rooms[roomid].users[username].pile;
-		// Send the top card of the pile
-		socket.emit("getTopCard", pile[0]);
-		// Pile is now empty. Tell player this.
-		if (pile.length === 1)
-			socket.emit("updatePile", true);
-	});
-
-	// User chose a card to discard and pass to the player on the left
-	socket.on("discard", function (index){
-		var username = session.username,
-			roomid = session.room,
-			users = rooms[roomid].users,
-			hand = users[username].hand;
-			pile = users[username].pile;
-		// Get the card based on the index (0-3 = hand, -1 = top card of pile)
-		var card;
-		if (index === "-1"){
-			card = pile.shift();
-		} else {
-			card = hand[index];
-			// Put top card in hand
-			hand[index] = pile.shift();
-		}
-		// Find user to the left, which should be a simple add one and modulo
-		var usernames = Object.keys(users),
-			playerSeat = usernames.indexOf(username),
-			leftSeat = (playerSeat + 1) % usernames.length,
-			leftPlayerName = usernames[leftSeat];
-			playerSocket = userToSocket[leftPlayerName];
-		// Put discarded card on end of pile (user.pile.push)
-		users[leftPlayerName].pile.push(card);
-		// At this point, if anyone's piles just changed from being empty, alert them
-		if (users[leftPlayerName].pile.length === 1)
-			playerSocket.emit("updatePile", false);
-	});
-
-	// User is attempting to get the spoon located at some index
-	socket.on("getSpoon", function (index){
-		var username = session.username,
-			roomid = session.room,
-			users = rooms[roomid].users,
-			usernames = Object.keys(users),
-			spoons = rooms[roomid].spoons,
-			hand = users[username].hand;
-		// Check if 4 of a Kind in hand or if the number of spoons is not at maximum
-		if (fourOfKind(hand) || spoons !== usernames.length - 1){
-			// If valid, confirm taking the spoon by telling all users
-			var userid = usernames.indexOf(username);
-			io.emit("removeSpoon", index, userid);
-			rooms[roomid].spoons--;
-			// If spoons is 0, send message to end game
-		} else {
-			// Otherwise, penalize the player
-			socket.emit("penalty");
-		}
-		
-		//io.emit("removeSpoon", index, thisUser);
-
-		
-		//socket.emit("penalty");
-	});
-
 
 });
 
@@ -309,7 +232,7 @@ app.post("/connect", function (req, res){
 // enough users are connected.
 setInterval(function(){
 	// If more than one user in the room
-	if (Object.keys(rooms[openRoomID].users).length > 1){
+	if (rooms[openRoomID].users.length > 1){
 		// Decrease the timer. If elapsed, forcibly start the game.
 		waitTime--;
 		if (waitTime === 0){
@@ -324,7 +247,7 @@ setInterval(function(){
 setInterval(function(){
 	//console.log("Games in progress: " + inGameRooms.length);
 	console.log("Open Room ID: " + openRoomID);
-	console.log("Number of users in this room: " + Object.keys(rooms[openRoomID].users).length);
+	console.log("Number of users in this room: " + rooms[openRoomID].users.length);
 	console.log("Wait time remaining: " + waitTime);
 }, 5000);
 
